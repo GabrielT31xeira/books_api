ba32421b2862a582d275c89bb510a1c1eab00dd6
Gabriel Teixeira de Carvalho <70416214+GabrielT31xeira@users.noreply.github.com>
2023-12-20 15:30:34 -0300

    Finish the book crud

---
+++ src/Controller/BookController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Book;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\BookRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+
+class BookController extends AbstractController
+{
+    #[Route('/books', name: 'book_list', methods: ['GET'])]
+    public function index(BookRepository $bookRepository): JsonResponse
+    {
+        // Verify if the books exists
+
+        if (!$bookRepository->findAll()) {
+            // if not found
+            return $this->json([
+                'message' => 'Books not foud'
+            ], 404);
+        } else {
+            //if find
+            return $this->json([
+                'data' => $bookRepository->findAll()
+            ], 200);
+        }
+    }
+
+    #[Route('/book/{book}', name: 'book_sigle', methods: ['GET'])]
+    public function sigle(int $book,BookRepository $bookRepository): JsonResponse
+    {
+        // Verify if the book exists
+
+        if (!$bookRepository->find($book)) {
+            // if not found
+            return $this->json([
+                'message' => 'Book not foud'
+            ], 404);
+        } else {
+            //if find
+            return $this->json([
+                'data' => $bookRepository->find($book)
+            ], 200);
+        }
+    }
+
+    #[Route('/book/{book}', name: 'book_update', methods: ['PUT', 'PATCH'])]
+    public function update(int $book, Request $request, ManagerRegistry $doctrine, BookRepository $bookRepository): JsonResponse
+    {
+        // Find if the book exist
+        if (!$bookRepository->find($book)) {
+            // if not found
+            return $this->json([
+                'message' => 'Book not foud'
+            ], 404);
+        } else {
+            //if find
+            $book = $bookRepository->find($book);
+        }
+
+        // Convert the request to json and get the content
+        $data = json_decode($request->getContent(), true);
+
+        // Update book to database
+        $book->setTitle($data['title']);
+        $book->setIsbn($data['isbn']);
+        $book->setUpdatedAt(new \DateTimeImmutable('now', new \DateTimeZone('America/Sao_Paulo')));
+
+        // Update the book
+        $doctrine->getManager()->flush();
+
+        // Return the response
+        return $this->json([
+            'message' => 'Book update successfully!',
+            'book' => $book
+        ], 200);
+    }
+
+    #[Route('/book', name: 'book_create', methods: ['POST'])]
+    public function create(Request $request, BookRepository $bookRepository): JsonResponse
+    {
+        // Convert the request to json and get the content
+        $data = json_decode($request->getContent(), true);
+
+        // Add new book to database
+        $book = new Book();
+        $book->setTitle($data['title']);
+        $book->setIsbn($data['isbn']);
+        $book->setCreatedAt(new \DateTimeImmutable('now', new \DateTimeZone('America/Sao_Paulo')));
+        $book->setUpdatedAt(new \DateTimeImmutable('now', new \DateTimeZone('America/Sao_Paulo')));
+
+        // finish the controller
+        $bookRepository->add($book, true);
+
+        return $this->json([
+            'message' => 'Book created successfully!',
+            'book' => $book
+        ], 201);
+    }
+}
