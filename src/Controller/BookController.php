6e15d01d3a5d8e87ed0523e20d13657375ae0e69
Gabriel Teixeira de Carvalho <70416214+GabrielT31xeira@users.noreply.github.com>
2023-12-28 16:16:48 -0300

    Add documentation for the project

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -9,48 +9,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\BookRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Nelmio\ApiDocBundle\Annotation\Model;
-use Nelmio\ApiDocBundle\Annotation\Security;
-use OpenApi\Annotations as OA;
 
 class BookController extends AbstractController
 {
-    /**
-     * @OA\Get(
-     *     path="/api/books",
-     *     tags={"Books"},
-     *     summary="Retrieves a list of all books",
-     *     description="Returns an array of book objects, or an error message if no books are found.",
-     *     @OA\Response(
-     *         response=200,
-     *         description="Success",
-     *         @OA\JsonContent(
-     *             type="array",
-     *             items={
-     *                 @OA\Property(
-     *                     property="id",
-     *                     type="integer",
-     *                     description="The book's ID"
-     *                 ),
-     *                 @OA\Property(
-     *                     property="title",
-     *                     type="string",
-     *                     description="The book's title"
-     *                 ),
-     *                 @OA\Property(
-     *                     property="isbn",
-     *                     type="string",
-     *                     description="The book's isbn"
-     *                 )
-     *             }
-     *         )
-     *     ),
-     *     @OA\Response(
-     *         response=404,
-     *         description="Books not found"
-     *     )
-     * )
-     */
     #[Route('/books', name: 'book_list', methods: ['GET'])]
     public function index(BookRepository $bookRepository): JsonResponse
     {
@@ -87,7 +48,29 @@ class BookController extends AbstractController
         }
     }
 
-    #[Route('/book/{book}', name: 'book_update', methods: ['PUT', 'PATCH'])]
+    #[Route('/book', name: 'book_create', methods: ['POST'])]
+    public function create(Request $request, BookRepository $bookRepository): JsonResponse
+    {
+        // Convert the request to json and get the content
+        $data = json_decode($request->getContent(), true);
+
+        // Add new book to database
+        $book = new Book();
+        $book->setTitle($data['title']);
+        $book->setIsbn($data['isbn']);
+        $book->setCreatedAt(new \DateTimeImmutable('now', new \DateTimeZone('America/Sao_Paulo')));
+        $book->setUpdatedAt(new \DateTimeImmutable('now', new \DateTimeZone('America/Sao_Paulo')));
+
+        // finish the controller
+        $bookRepository->add($book, true);
+
+        return $this->json([
+            'message' => 'Book created successfully!',
+            'book' => $book
+        ], 201);
+    }
+
+    #[Route('/book/{book}/update', name: 'book_update', methods: ['PUT', 'PATCH'])]
     public function update(int $book, Request $request, ManagerRegistry $doctrine, BookRepository $bookRepository): JsonResponse
     {
         // Find if the book exist
@@ -119,29 +102,7 @@ class BookController extends AbstractController
         ], 200);
     }
 
-    #[Route('/book', name: 'book_create', methods: ['POST'])]
-    public function create(Request $request, BookRepository $bookRepository): JsonResponse
-    {
-        // Convert the request to json and get the content
-        $data = json_decode($request->getContent(), true);
-
-        // Add new book to database
-        $book = new Book();
-        $book->setTitle($data['title']);
-        $book->setIsbn($data['isbn']);
-        $book->setCreatedAt(new \DateTimeImmutable('now', new \DateTimeZone('America/Sao_Paulo')));
-        $book->setUpdatedAt(new \DateTimeImmutable('now', new \DateTimeZone('America/Sao_Paulo')));
-
-        // finish the controller
-        $bookRepository->add($book, true);
-
-        return $this->json([
-            'message' => 'Book created successfully!',
-            'book' => $book
-        ], 201);
-    }
-
-    #[Route('/book/{book}', name: 'book_delete', methods: ['DELETE'])]
+    #[Route('/book/{book}/delete', name: 'book_delete', methods: ['DELETE'])]
     public function delete(int $book, BookRepository $bookRepository): JsonResponse
     {
         // Verify if the book exists
